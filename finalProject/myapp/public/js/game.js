3bd105fd0287ffc01b2541ce28131b7c15d49ae4
Tiffanyiong <tiong@shaeba.net>
2020-12-15 12:29:30 -0500

    fix game bug and add play game btn

--- finalProject/myapp/public/js/game.js
+++ finalProject/myapp/public/js/game.js
@@ -3,21 +3,24 @@ const ctx = canvas.getContext("2d");
 canvas.width=800;
 canvas.height=480;
 
+
+
 let spacePressed = false;
 let angle = 0
 let hue = 0;
 let score = 0;
 let frame=0;
-let gameSpeed= 3;
+let gameSpeed= 2;
 
 
 
-let temp = canvas.height-90;
+
  function animate() {
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     handleObstacles();
-    bird.update();
+    bird.update();  
     bird.draw();
     ctx.font = '90px Arial Black';
     ctx.fillStyle ="#FFD42A";
@@ -31,8 +34,17 @@ let temp = canvas.height-90;
     angle+=0.12;
     frame++;
 };
-animate();
+// animate();
 
+//prevent "space bar scrolling"
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = e.keyCode || e.which;
+    if (charCode === 32) {
+        e.preventDefault();
+        return false;
+    }
+}
 window.addEventListener('keydown', function(e){
     if(e.code === 'Space') spacePressed = true;         
 
@@ -65,6 +77,8 @@ function handleBang(){
     }
 }
 
-
-
-
+function playgame(){
+    var playbtn = document.getElementById("playBtn");
+    playbtn.style.display="none";
+    animate();
+}
